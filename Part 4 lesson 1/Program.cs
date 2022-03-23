516865620f9e2f1c86496bef948d30c872997797
guildwars976@gmail.com <guildwars976@gmail.com>
2022-03-23 19:52:29 +0300

    Добавьте файлы проекта.

---
+++ Part 4 lesson 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part_4_lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
