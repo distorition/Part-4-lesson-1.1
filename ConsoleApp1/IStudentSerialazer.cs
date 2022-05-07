9309d6e4c376ce80269536f8c2167cc9f0b0611b
guildwars976@gmail.com <guildwars976@gmail.com>
2022-05-07 10:56:40 +0300

    проблема с сериализацией Джейсон

---
+++ ConsoleApp1/IStudentSerialazer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    public interface IStudentSerialazer//штука для одоптации всех трех лкасс Сериализации 
+    {
+        void Serialaized(Stream textWriter,List<Student> students);
+       List<Student> Diserialaized(Stream textReader);
+
+    }
+    public abstract class StudentSerialazer : IStudentSerialazer//мы реализовали абстрактный класс за тем чтобы реализовать в нем какие то общие методы
+    {
+        public abstract List<Student> Diserialaized(Stream textReader);
+        public abstract void Serialaized(Stream textWriter, List<Student> students);
+    }
+
+    internal class XmlStudentSerialazer : StudentSerialazer//класс для сериализации в формате Xml
+    {
+        private static readonly XmlSerializer xmlSerilazer= new XmlSerializer(typeof(List<Student>));// XmlSerialazer всегда очень долго создается 
+        public override List<Student> Diserialaized(Stream textReader)
+        {
+            return (List<Student>?)xmlSerilazer.Deserialize(textReader) ?? throw new InvalidOperationException("Не удалось получить список студентов ");//если в наш список будет равен Null то мы получим ошибку 
+        }
+
+        public override void Serialaized(Stream textWriter, List<Student> students)
+        {
+            xmlSerilazer.Serialize(textWriter, students);
+        }
+    }
+
+    internal class BinarySerialazedStudent : StudentSerialazer// класс для сериализации в формате  Binary
+    {
+        private static readonly BinaryFormatter xmlSerilazer = new BinaryFormatter();
+        public override List<Student> Diserialaized(Stream textReader)
+        {
+            return (List<Student>?)xmlSerilazer.Deserialize(textReader) ?? throw new InvalidOperationException("Не удалось получить список студентов ");//если в наш список будет равен Null то мы получим ошибку 
+        }
+
+        public override void Serialaized(Stream textWriter, List<Student> students)
+        {
+            xmlSerilazer.Serialize(textWriter, students);
+        }
+    }
+
+    /// <summary>
+    /// тут есть проблема ибо джейсон почему то не принимает лист со студентами
+    /// </summary>
+    //internal class JsonSerialazedStudent : StudentSerialazer// класс для сериализации в формате  Json
+    //{
+    
+    //    public override List<Student> Diserialaized(Stream textReader)
+    //    {
+    //        return JsonSerializer.Deserialize<List<Student>>(textReader) ?? throw new InvalidOperationException("Не удалось получить список студентов ");//если в наш список будет равен Null то мы получим ошибку 
+    //    }
+    
+
+    //    public override void Serialaized(Stream textWriter, List<Student>students)
+    //    {
+           
+    //        JsonSerializer.Serialize(textWriter, students);
+    //    }
+    //}
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ConsoleApp1
 {
@@ -27,7 +30,38 @@ namespace ConsoleApp1
             //var ttask= AsyncAwaitTest.RunAsync();// таким образом если выскочит ошибка то она будет хранится в переменной и не положит нашу программу 
             // await AsyncAwaitTest.Run2Async();
             //LogingExamples.Run();
-            BuilderPattern.Run();
+            //BuilderPattern.Run();
+
+            //var student = new Student()
+            //{
+            //    Name = "Ivanov",
+            //    SecondName = "Ivan",
+            //    Id = 1,
+            //    Patromic = "Ivanovich",
+            //    Birthday =  DateTime.Now.AddYears(-18)
+            //};
+            //var xmlSer = new XmlSerializer(typeof(Student));//сериализуем нащего студента
+            //const string xmlFileName = "student.xml";
+            //using (var xmlFile = File.Create(xmlFileName))
+            //    xmlSer.Serialize(xmlFile, student);
+
+            //Student? student1 = null;
+            //using (var xmlFile = File.OpenText(xmlFileName))
+            //{
+            //    student1=(Student?) xmlSer.Deserialize(xmlFile);
+            //}
+            var xmlSerialezd = new XmlStudentSerialazer();
+            var binarySerialaized = new BinarySerialazedStudent();
+
+            var student_Manager= new StudentManager(xmlSerialezd);
+            student_Manager.Add("Ivanov","ivan","Ivanovic",DateTime.Now.AddYears(-18));
+            student_Manager.Add("Andrey", "Evdakimovich", "Ivanovic", DateTime.Now.AddYears(-18));
+            student_Manager.Add("Anton", "Gandon", "Ivanovic", DateTime.Now.AddYears(-18));
+
+            const string file_name = "student.xml";
+            student_Manager.SvaeTo(file_name);
+            //XmlSerializer serializer = new XmlSerializer(typeof(Student));
+            //BinaryFormatter binary= new BinaryFormatter();
         }
     }
 }
