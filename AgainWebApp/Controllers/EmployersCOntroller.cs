24303312c08e2abfad4e5b59a7fc0eec3e135d1b
guildwars976@gmail.com <guildwars976@gmail.com>
2022-09-02 18:26:04 +0300

    Создание сотрудника

--- AgainWebApp/Controllers/EmployersCOntroller.cs
+++ AgainWebApp/Controllers/EmployersCOntroller.cs
@@ -1,4 +1,5 @@
-﻿using AgainWebApp.Services.Interfaces;
+﻿using AgainWebApp.Models;
+using AgainWebApp.Services.Interfaces;
 using AgainWebApp.Services.ViewsModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -38,9 +39,108 @@ namespace AgainWebApp.Controllers
         });//добавялем нашу вью модель
         }
 
-        public IActionResult Create()=>View();
-        public IActionResult Edit() => View();
-        public ActionResult Delete() => View();
+        public IActionResult Create()=>View("Edit",new EmployersViewModel());//вызывает персдтавления Edit и передаёт ему пустую вью модель
 
+
+        // так же формируем модель представления Razor и шаблон указываем наш контроллер то есть ( EmployersCOntroller)
+        //в самом шаблоне есть два действия которая будет отправлять форму  нашего человека на кнопку Edit и вторая  которая будет принимать 
+        public IActionResult Edit(int? id )// отправляет 
+        {
+            if(id is not null)
+            {
+                return View(new EmployersViewModel());
+            }
+            var employer = _EmployersStore.GetByID((int)id);//ищем пользвоателя по ид
+
+            if (employer is null)//если не нашли то посылаем ошибку 404 
+            {
+                return NotFound();
+            }
+
+            return View(new EmployersViewModel//если нашли то отправялем ему нашего чела которого будем редактирвоать 
+            {
+                Id = employer.Id,
+                LastName = employer.LastName,
+                FirstName = employer.FirstName,
+                Patronomic = employer.Patronomic,
+                Birthday = employer.Birthday,
+            });//добавялем нашу вью модель
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployersViewModel model)// принимает ,принимаем модель которая уже отредактирвоана  
+        {
+            //var employer= _EmployersStore.GetByID(model.Id);
+            //if (employer is null)//если не нашли то посылаем ошибку 404 
+            //{
+            //    return NotFound();
+            //}
+
+            var employee= new Employer
+            {
+                Id = model.Id,
+                LastName = model.LastName,
+                FirstName = model.FirstName,
+                Patronomic = model.Patronomic,
+                Birthday = model.Birthday,
+            };
+
+            if(employee.Id== 0)//если ид=0 значит это новый сотрудник 
+            {
+                var id = _EmployersStore.Add(employee);
+                return RedirectToAction("Details", new { id });
+            }
+
+            var succes = _EmployersStore.Edit(employee);
+
+            if (!succes)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("Index");//возвращаем редирект на дейсвие индекс 
+        }
+
+        public ActionResult Delete(int id)// первый запрос будет находить и формировать нашу модель 
+        {
+            var employer = _EmployersStore.GetByID(id);//ищем пользвоателя по ид
+
+            if (employer is null)//если не нашли то посылаем ошибку 404 
+            {
+                return NotFound();
+            }
+
+
+            return View(new EmployersViewModel// тут мы ввозвращаем нашу сформированную модель 
+            {
+                Id = employer.Id,
+                LastName = employer.LastName,
+                FirstName = employer.FirstName,
+                Patronomic = employer.Patronomic,
+                Birthday = employer.Birthday,
+            });
+        }
+
+        [HttpPost]//тут нужен пост запрос для безопасности чтобы никто не смог взломать и удалиить наших сотрудников 
+        public ActionResult DeleteConfirmed(int id)//второй будет принимать эту модель и удалять её
+        {
+
+            //var employer = _EmployersStore.GetByID(id);//ищем пользвоателя по ид
+
+            //if (employer is null)//если не нашли то посылаем ошибку 404 
+            //{
+            //    return NotFound();
+            //}
+
+            //_EmployersStore.Remove(id);
+
+            if (!_EmployersStore.Remove(id))//если не удалось удалить значит он не был найден 
+            {
+                return NotFound();
+            }
+
+
+            return RedirectToAction("Index");//а если удалось удалить то возвращаем редирект 
+        }
     }
 }
--- AgainWebApp/Services/ViewsModels/EmployersViewModel.cs
+++ AgainWebApp/Services/ViewsModels/EmployersViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace AgainWebApp.Services.ViewsModels
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgainWebApp.Services.ViewsModels
 {
     public class EmployersViewModel
     {
+        [HiddenInput(DisplayValue =false)]//таким образом мы скрываем id человека от пользователей 
         public int Id { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Patronomic { get; set; }
